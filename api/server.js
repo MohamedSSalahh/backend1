dec14c6bf613b143047b8a38418ce39abc1d3673
mohamedsalah <hsalahxxx@gmail.com>
2025-11-24 20:32:11 +0200

    "okay"

--- api/server.js
+++ api/server.js
@@ -1,6 +1,5 @@
-// lambda.js
 const serverless = require("serverless-http");
-const app = require("./app"); // path to your app.js
+const app = require("../app"); // path to your app.js
 const mongoose = require("mongoose");
 require("dotenv").config();
 
@@ -14,21 +13,22 @@ async function connectDB() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URI).then((mongoose) => mongoose);
+    cached.promise = mongoose
+      .connect(process.env.MONGO_URI)
+      .then((mongoose) => mongoose);
   }
   cached.conn = await cached.promise;
   return cached.conn;
 }
 
-// Wrap your app in serverless once
-const handler = serverless(app);
+const serverlessHandler = serverless(app);
 
-module.exports.handler = async (req, res) => {
+module.exports = async (req, res) => {
   try {
     await connectDB();
-    return handler(req, res); // call serverless handler
-  } catch (err) {
-    console.error(err);
+    return serverlessHandler(req, res);
+  } catch (error) {
+    console.error(error);
     res.status(500).send("Internal Server Error");
   }
 };
